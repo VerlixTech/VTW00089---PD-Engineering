a7cf07c6509a87ed9ec074f655cb0d564a86f221
IsuruMJA99 <mjaisurumadusanka@gmail.com>
2026-03-10 16:19:07 +0530

    create footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Marck_Script } from 'next/font/google';
+import Footer from "@/components/footer";
+import { Header } from "@/components/header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -33,7 +35,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${markScript.variable} antialiased`}
       >
+        <Header />
+
         {children}
+        
+        <Footer />
       </body>
     </html>
   );
